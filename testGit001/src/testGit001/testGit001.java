a8f334edccd072ccf8604fad91fbe0968e1d202f
kibbm <kibbmcc@gmail.com>
2014-07-02 21:14:58 +0900

    ..

---
+++ testGit001/src/testGit001/testGit001.java
@@ -0,0 +1,12 @@
+package testGit001;
+
+public class testGit001 {
+
+	public testGit001() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hi");
+	}
+}
